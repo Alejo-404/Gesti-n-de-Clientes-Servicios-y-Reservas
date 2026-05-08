7565248baf8bb0a763f894c3f57d76de38203c99
ChristianFDelgado <cristianfabiandelgado@gmail.com>
2026-05-07 20:59:20 -0500

    Miguel implementa creación de servicios válidos e inválidos

--- EjecutableGestion.py
+++ EjecutableGestion.py
@@ -1,1 +1,39 @@
-# Mensaje para el equipo: Aquí deben poner sus casos de uso
+# =========================================
+# OPERACIÓN 03 - CREACIÓN SERVICIO VÁLIDO
+# =========================================
+
+class ReservaSala:
+    def __init__(self, horas):
+
+        if horas <= 0:
+            raise ValueError("Horas inválidas")
+
+        self.horas = horas
+
+    def calcular_costo(self):
+        return self.horas * 50
+
+
+# ==========================
+# SERVICIO VÁLIDO
+# ==========================
+
+try:
+    servicio_valido = ReservaSala(2)
+
+    print("Servicio válido creado correctamente")
+    print("Costo:", servicio_valido.calcular_costo())
+
+except Exception as e:
+    print("Error:", e)
+
+
+# ==========================
+# SERVICIO INVÁLIDO
+# ==========================
+
+try:
+    servicio_invalido = ReservaSala(-1)
+
+except Exception as e:
+    print("Error servicio inválido:", e)
