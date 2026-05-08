90deee190b495fd9964be57c3a753abd2c036eee
ChristianFDelgado <cristianfabiandelgado@gmail.com>
2026-05-07 21:11:44 -0500

    Miguel implementa creación de servicio inválido

--- EjecutableGestion.py
+++ EjecutableGestion.py
@@ -1,1 +1,29 @@
-# Mensaje para el equipo: Aquí deben poner sus casos de uso
+# =========================================
+# OPERACIÓN 04 - CREACIÓN SERVICIO INVÁLIDO
+# =========================================
+
+class ReservaSala:
+    def __init__(self, horas):
+
+        if horas <= 0:
+            raise ValueError("Horas inválidas")
+
+        self.horas = horas
+
+    def calcular_costo(self):
+        return self.horas * 50
+
+
+# ==========================
+# SERVICIO INVÁLIDO
+# ==========================
+
+try:
+
+    servicio_invalido = ReservaSala(-5)
+
+    print("Servicio creado correctamente")
+
+except Exception as e:
+
+    print("Error servicio inválido:", e)
