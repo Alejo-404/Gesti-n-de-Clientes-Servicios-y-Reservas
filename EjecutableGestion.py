bb872b6f5073262ae6ac03b5faccdf14232dcceb
ChristianFDelgado <cristianfabiandelgado@gmail.com>
2026-05-07 22:48:47 -0500

    Miguel implementa servicio válido con POO

--- EjecutableGestion.py
+++ EjecutableGestion.py
@@ -2,15 +2,18 @@
 # OPERACIÓN 03 - CREACIÓN SERVICIO VÁLIDO
 # =========================================
 
-class ReservaSala:
-    def __init__(self, horas):
+class Servicio:
+
+    def __init__(self, nombre, horas):
 
         if horas <= 0:
             raise ValueError("Horas inválidas")
 
+        self.nombre = nombre
         self.horas = horas
 
     def calcular_costo(self):
+
         return self.horas * 50
 
 
@@ -19,21 +22,13 @@ class ReservaSala:
 # ==========================
 
 try:
-    servicio_valido = ReservaSala(2)
+
+    servicio_valido = Servicio("Reserva Sala", 2)
 
     print("Servicio válido creado correctamente")
+    print("Servicio:", servicio_valido.nombre)
     print("Costo:", servicio_valido.calcular_costo())
 
 except Exception as e:
+
     print("Error:", e)
-
-
-# ==========================
-# SERVICIO INVÁLIDO
-# ==========================
-
-try:
-    servicio_invalido = ReservaSala(-1)
-
-except Exception as e:
-    print("Error servicio inválido:", e)
