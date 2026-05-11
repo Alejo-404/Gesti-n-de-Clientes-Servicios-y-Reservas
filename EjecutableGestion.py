9738fbc9287240af5d24d092b67cc915c2c4aeb4
qamii259 <davidscoups78@gmail.com}>
2026-05-11 18:14:31 -0500

    added invalid customer validation

--- EjecutableGestion.py
+++ EjecutableGestion.py
@@ -1,5 +1,7 @@
-# Mensaje para el equipo: Aquí deben poner sus casos de uso
-def _init_(self, name, email):
+# Mensaje para el equipo: Aquí deben poner sus caso
+class Customer:
+
+    def __init__(self, name, email):
 
         if not name.strip():
 
@@ -16,10 +18,46 @@ def _init_(self, name, email):
         self.name = name
         self.email = email
 
-def show_info(self):
+    def show_info(self):
 
         return (
             f"Customer: {self.name} | "
             f"Email: {self.email}"
         )
+
+
+print("\nCUSTOMER SYSTEM\n")
+
+
+# VALID CUSTOMER
+try:
+
+    customer1 = Customer(
+        "Camila",
+        "camila@gmail.com"
+    )
+
+    print("\nValid Customer Created")
+    print(customer1.show_info())
+
+except Exception as e:
+
+    print(f"Error: {e}")
+
+
+# INVALID CUSTOMER
+try:
+
+    customer2 = Customer(
+        "",
+        "wrongemail"
+    )
+
+    print(customer2.show_info())
+
+except Exception as e:
+
+    print("\nInvalid Customer Detected")
+    print(f"Error: {e}")
+    
         
